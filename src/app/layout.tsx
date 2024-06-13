9d3d33a4008f7f8316ba11e453bdbb6cb3aefec0
bidanee <dleksql119@gmail.com>
2024-06-13 17:21:22 +0900

    login edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Link from "next/link"
+import { logout } from "@/libs/action"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,7 +23,9 @@ export default function RootLayout({
         <Link href="/login">login</Link>
         <Link href="/register">register</Link>
         <Link href="/dashboard">DashBoard</Link>
-
+        <form action={logout}>
+          <button>로그아웃</button>
+        </form>
         {children}
       </body>
     </html>
